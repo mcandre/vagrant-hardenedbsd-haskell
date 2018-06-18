9fb8e57798d74ac29f75337661b029ce429b2ccd
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-06-17 21:18:56 -0500

    keep up with repository

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo pkg update &&
+sudo hbsd-update &&
+    sudo pkg update &&
     sudo pkg install -y hs-cabal-install &&
     sudo pkg clean -y &&
     sudo sed -i -e 's/:path=\/sbin \/bin \/usr\/sbin \/usr\/bin \/usr\/local\/sbin \/usr\/local\/bin ~\/bin:\\/:path=\/sbin \/bin \/usr\/sbin \/usr\/bin \/usr\/local\/sbin \/usr\/local\/bin ~\/bin \/home\/vagrant\/.cabal\/bin:\\/' /etc/login.conf &&
